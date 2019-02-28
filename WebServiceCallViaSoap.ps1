2f0cf07ad95c44c5902eb8bc3b289fc5c2f217d1
Nibu Babu Thomas <nthomas@microsoft.com>
2019-02-28 09:52:33 -0600

    fixed soapbody and removed server name

--- WebServiceCallViaSoap.ps1
+++ WebServiceCallViaSoap.ps1
@@ -2,7 +2,7 @@
 $domain = "kesar"
 $user = "Administrator"
 $fqusername = "$domain\$user"
-$pwdstring = Read-Host -Prompt "Please enter password for $fqusername : " -AsSecureString
+$pwdstring = Read-Host -Prompt "Please enter password for $fqusername" -AsSecureString
 if($null -eq $pwdstring)
 {
     Write-Error "Invalid password entered"
@@ -11,22 +11,22 @@ if($null -eq $pwdstring)
 $creds = New-Object System.Management.Automation.PSCredential($fqusername, $pwdstring)
 
 # Prepare SOAP message
-$url = "http://nt-sp2013-26/_vti_bin/lists.asmx"
+$url = "http://server/_vti_bin/sitedata.asmx"
+
 $soapbody = @"
 <?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-    <soap:Body><GetListCollection xmlns="http://schemas.microsoft.com/sharepoint/soap/" />
-    </soap:Body>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
+  <soap:Body>
+    <GetSite xmlns="http://schemas.microsoft.com/sharepoint/soap/" />
+  </soap:Body>
 </soap:Envelope>
 "@
 
 #Prepare SOAP Header...
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("User-Agent", "Mozilla/4.0")
-$headers.Add("Content-Type", 'text/xml;charset=UTF-8')
-$headers.Add("SOAPAction", "http://schemas.microsoft.com/sharepoint/soap/GetListCollection")
+$headers.Add("Content-Type", 'application/soap+xml;charset=UTF-8')
 $headers.Add("Content-Length", $soapbody.Length);
-$headers.Add("Transfer-Encoding", "Chunked");
 
 #Send the request
 $lists = Invoke-WebRequest -Method Post -Uri $url -Headers $headers -Body $soapbody -SessionVariable sessionvar -Credential $creds

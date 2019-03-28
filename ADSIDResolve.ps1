5699c409b2ac3503e1955b0b05da457f9cf50a3e
Nibu Babu Thomas <nthomas@microsoft.com>
2019-03-28 09:07:08 -0500

    Removed hard coded values, added sanity checks

--- ADSIDResolve.ps1
+++ ADSIDResolve.ps1
@@ -1,3 +1,26 @@
+# Verify file status #
+$maxparalleljobs = 200
+$continue        = $false
+$filePath        = "c:\sids.txt" #make sure this file contains SIDs that needs to be resolved.
+
+if((Test-Path $filePath) -eq $false)
+{
+    Write-Error -Message "File $filePath does not exist. Please correct file path before using this script. Exiting!"
+    return
+}
+
+# Count of lines to show progress
+$sidlines = Get-Content -Path $filePath
+$totalLines = ( $sidlines | Measure-Object -Line).Lines
+
+if(0 -ge $totalLines)
+{
+    Write-Error -Message "File $filePath empty?! This file must contain the SIDs you're intending to resolve! Exiting!"
+    return    
+}
+
+
+#Script block which will be used by Powershell Jobs
 $ADSidResolve = {
     param($sid)
     Write-Host "SID: $sid"
@@ -6,26 +29,23 @@ $ADSidResolve = {
     $objUser.Value
 }
 
+#Read in UserName and Password securely
 $username = Read-Host -Prompt "Please enter username: "
 $pwd = Read-Host -Prompt "Please enter your password: " -AsSecureString
 $cred = New-Object System.Management.Automation.PSCredential($username,$pwd)
 if($null -eq $cred)
 {
+    Write-Error -Message "Invalid credentials! Exiting!"
     return;
 }
 
-$maxparalleljobs = 200
-$continue        = $false
-$filePath        = "c:\sids.txt" #make sure this file contains SIDs that needs to be resolved.
-
-#Count of lines to show progress
-$totalLines = (Get-Content -Path $filePath | Measure-Object -Line).Lines
-
 $alljobs = @()
-$reader = [System.IO.File]::OpenText("$filePath")
 $curLine = 0
-while($null -ne ($line = $reader.ReadLine().Trim())) 
+foreach($line in $sidlines)
 {
+    #Trim off spaces from either sides of read in string
+    $line = $line.Trim()
+
     ++$curLine;
     $continue = $false
     while($continue -eq $false)
@@ -33,13 +53,13 @@ while($null -ne ($line = $reader.ReadLine().Trim()))
         $continue = ((Get-Job -State "Running").Count -lt $maxparalleljobs)
         if($continue)
         {
-            $JobName = "PSJOB--$line"    
+            $JobName = "PSJOB-SIDResolve-$line"    
             $alljobs += Start-Job -ScriptBlock $ADSIDResolve -Name $JobName -Credential $cred -ArgumentList $line
         }
         else
         {
             #Yield control for half a second
-            Start-Sleep -Milliseconds 100
+            #Start-Sleep -Milliseconds 10
         }
     }
 
@@ -47,11 +67,8 @@ while($null -ne ($line = $reader.ReadLine().Trim()))
     Write-Progress -Activity "Progress resolving SID... (Current line: $curLine of $totalLines)" -Status "$PercentComplete% Complete:" -PercentComplete $PercentComplete
 }
 
-$reader.Close()
-
 #Wait for jobs to finish, in the end print the output
 Write-Host "Waiting on jobs to finish..."
 Get-Job | Wait-Job | Receive-Job
 
-#Write-Host "`nAll Jobs and their state..."
-#$alljobs | Select-Object Name,State
+Remove-Variable alljobs,maxparalleljobs

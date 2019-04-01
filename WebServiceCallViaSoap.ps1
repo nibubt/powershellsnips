bf6d4c37e901e10f12e24203275f121e4d0d0768
Nibu Babu Thomas <nthomas@microsoft.com>
2019-04-01 10:09:38 -0500

    removed hard coded vars

--- WebServiceCallViaSoap.ps1
+++ WebServiceCallViaSoap.ps1
@@ -1,6 +1,6 @@
-#credentials, change user name if needed
-$domain = "kesar"
-$user = "Administrator"
+#Credentials, change user name if needed
+$domain = Read-Host -Prompt "Please enter domain"
+$user =  Read-Host -Prompt "Please enter username"
 $fqusername = "$domain\$user"
 $pwdstring = Read-Host -Prompt "Please enter password for $fqusername" -AsSecureString
 if($null -eq $pwdstring)
@@ -9,17 +9,17 @@ if($null -eq $pwdstring)
     return
 }
 $creds = New-Object System.Management.Automation.PSCredential($fqusername, $pwdstring)
-
-# Prepare SOAP message
+  
+#Prepare SOAP message
 $url = "http://server/_vti_bin/sitedata.asmx"
 
 $soapbody = @"
 <?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
-  <soap:Body>
-    <GetSite xmlns="http://schemas.microsoft.com/sharepoint/soap/" />
-  </soap:Body>
-</soap:Envelope>
+<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+  <soap12:Body>
+    <GetListCollection xmlns="http://schemas.microsoft.com/sharepoint/soap/" />
+  </soap12:Body>
+</soap12:Envelope>
 "@
 
 #Prepare SOAP Header...
@@ -29,8 +29,8 @@ $headers.Add("Content-Type", 'application/soap+xml;charset=UTF-8')
 $headers.Add("Content-Length", $soapbody.Length);
 
 #Send the request
-$lists = Invoke-WebRequest -Method Post -Uri $url -Headers $headers -Body $soapbody -SessionVariable sessionvar -Credential $creds
+$lists = Invoke-WebRequest -Method "POST" -Uri $url -Headers $headers -Body $soapbody -Credential $creds
 
 #Dump output
 $xml = [xml]$lists.Content
-$xml.Envelope.Body.GetListCollectionResponse.GetListCollectionResult.Lists | ForEach-Object{$_.List} | Select-Object Title, ID | Format-Table -AutoSize
+$xml.Envelope.Body.GetListCollectionResponse.vLists | ForEach-Object{$_._sList} | Format-Table -AutoSize
